b33b4d09f8d0a32f02115e6c429ac057910605ba
riddhi22 <guptariddhi1997@gmail.com>
2017-01-25 19:49:43 +0530

    login logoutfav

--- app25/urls.py
+++ app25/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login,logout
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-	url(r'^post/new/$', views.post_new, name='post_new'),
-	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/favourities/(?P<pk>\d+)/$',views.add_fav,name='add_fav'),
+    url(r'^accounts/register/$',views.register, name='register'),
+    url(r'^accounts/register/complete/$', views.registration_complete,name='registration_complete'),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app25',
+    #'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -50,6 +51,14 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+#REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    #'DEFAULT_PERMISSION_CLASSES': [
+        #'rest_framework.permissions.IsAdminUser',
+    #],
+    #'PAGE_SIZE': 10
+#}
 
 ROOT_URLCONF = 'mysite.urls'
 
@@ -121,3 +130,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/accounts/loggedin/'
